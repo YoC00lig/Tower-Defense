13918f19c386a54c86b8c3b8e12f170b3bd281fc
YoC00lig <kulig.j@vp.pl>
2023-01-24 19:24:44 +0100

    Play button

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,6 +2,7 @@ package agh.ics.oop;
 import  agh.ics.oop.gui.App;
 import javafx.application.Application;
 
+
 public class World {
     public static void main(String[] args) {
         Application.launch(App.class);
